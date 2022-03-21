8b5598f3a662b683c85241c3ddcf927556ea4f98
laureLeNouail <laure.lenouail@gmail.com>
2022-03-21 15:34:54 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Network } from '@capacitor/network';
+import { Toast } from '@capacitor/toast';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
+
 export class Tab1Page {
 
+  
+
+
   constructor() {}
+
+  ngOnInit(){
+    this.showStatus();
+
+  }
+
+  
+  async showStatus(){
+    const status = await Network.getStatus();
+
+    await Toast.show({
+      text: "Le réseau est "+status.connectionType,
+    });
+
+  } 
+
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-tab2',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+
+
+  ngOnInit(){
+    this.printCurrentPosition();
+    let date = new Date();
+
+    console.log(date);
+  }
+
+  async printCurrentPosition(){
+    const coordinates = await Geolocation.getCurrentPosition();
+  
+    let latitude =coordinates.coords.latitude;
+  }
 
 }
